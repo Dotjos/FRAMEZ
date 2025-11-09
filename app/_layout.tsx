7cfadeb38e4db766a9c9709e5a3bfb13a41077e7
Jose <oladotjos@gmail.com>
2025-11-09 16:39:26 +0100

    authentication/UI:zustand store integration-done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,38 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useAuthStore } from "../store/authStore";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function ProtectedLayout() {
+  const router = useRouter();
+  const { user, fetchUser, initialized, loading } = useAuthStore();
+
+  useEffect(() => {
+    const init = async () => {
+      await fetchUser();
+    };
+    init();
+  }, []);
+
+  useEffect(() => {
+    if (initialized && !user) {
+      // user not logged in → redirect to login
+      router.replace("/login");
+    }
+  }, [user, initialized]);
+
+  if (loading && !initialized) {
+    return (
+      <View className="flex-1 items-center justify-center bg-white">
+        <ActivityIndicator size="large" color="#E1306C" />
+      </View>
+    );
+  }
+
+  return (
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeAreaProvider>
+  );
 }
